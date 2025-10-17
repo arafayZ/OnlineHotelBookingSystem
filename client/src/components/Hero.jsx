395edd3edddd0e16996bdc2714be9f947093138d
arafayZ <zahidabdul.4219@gmail.com>
2025-10-17 17:42:34 +0500

    Initial commit

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage.jpg")] bg-no-repeat bg-cover bg-center h-screen'>
+      
+    </div>
+  )
+}
+
+export default Hero
